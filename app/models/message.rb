7210d8d61ac1ea7f97a739a3d06d41834e6c31a2
R. P. Dillon <rpdillon@killring.org>
2015-07-18 07:46:35 -0700

    Add Message validations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,9 @@
 class Message < ActiveRecord::Base
+  validates :source, presence: true
+  validates_format_of :source, with: /[a-f0-9]{64}/
+  validates :destination, presence: true
+  validates_format_of :destination, with: /[a-f0-9]{64}/
+  validates :metadata, presence: true
+  validates :body, presence: true
+
 end
